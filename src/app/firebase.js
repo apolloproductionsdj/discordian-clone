742f63bfdcff23ae438ea411e5aff4cdd5438b61
mike <apolloproductionsdj@gmail.com>
2021-01-01 09:39:07 -0700

    Initial commit and cleanup process done for redux app

---
+++ src/app/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA7qHtQS90GemoOFA2SaEznZXwp17eRcgk",
+    authDomain: "discordian-clone.firebaseapp.com",
+    projectId: "discordian-clone",
+    storageBucket: "discordian-clone.appspot.com",
+    messagingSenderId: "697461584159",
+    appId: "1:697461584159:web:9f077821b681af02f8c02a",
+    measurementId: "G-T8RZFXDDL2"
+};
